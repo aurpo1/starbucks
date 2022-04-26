7959bc01118846eabb9c1b37b7c117282ca543a8
aurpo1 <aurpo1@naver.com>
2022-04-26 22:25:33 +0900

    feat: add badge
    badge들을 왼쪽 상단에 고정시켰다.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
 
-
 searchInputEl.addEventListener('focus', function() {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
 });
+
 searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
