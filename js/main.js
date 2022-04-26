45ae557eaebb3a4ec2f0363c1f1a4fe9484db231
aurpo1 <aurpo1@naver.com>
2022-04-26 22:43:11 +0900

    feat: modify badge
    아까는 가짜였습니다. 이게 진짜임.

--- js/main.js
+++ js/main.js
@@ -22,10 +22,17 @@ const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function() {
   if(window.scrollY > 500) {
     //배지 숨기기
-    badgeEl.style.display = 'none';
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
   } else {
     //배지 보이기
-    badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'block'
+    })
   } 
 }, 300));
 // _.throttle(함수, 시간)
